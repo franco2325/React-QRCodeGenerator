7c9d464a3d773c86d508cc810e55e1b60017dc2b
Franco Zeballos <91703556+franco2325@users.noreply.github.com>
2022-07-04 13:47:30 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles.css'
+import QrCode from './components/QrCode'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className='section container'>
+      <QrCode />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='footer'>
+      Franco Zeballos Dev.
+      <br />
+      <div className='link'>
+        <a href='https://github.com/franco2325'>See my Github</a>
+      </div>
+    </div>
+  )
+}
+
+export default footer
